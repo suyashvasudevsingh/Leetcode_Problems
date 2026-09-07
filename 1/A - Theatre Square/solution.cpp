3a0411814a1b2fcd75d4d3d8c14afc8052373dff
suyashvasudevsingh <work.suyash09@gmail.com>
2026-09-07 23:54:29 +0530

    Add Theatre Square [A] from Codeforces

---
+++ 1/A - Theatre Square/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    long long n, m, a;
+    cin >> n >> m >> a;
+ 
+    long long x = (n + a - 1) / a;
+    long long y = (m + a - 1) / a;
+ 
+    cout << x * y << endl;
+ 
+    return 0;
+}
