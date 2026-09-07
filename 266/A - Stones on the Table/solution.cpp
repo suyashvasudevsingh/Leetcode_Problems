219b0876e9470e14b3dd66dcbc29ae3348d9287f
suyashvasudevsingh <work.suyash09@gmail.com>
2026-09-07 23:55:03 +0530

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    string s;
+ 
+    cin >> n >> s;
+ 
+    int ans = 0;
+ 
+    for (int i = 1; i < n; i++) {
+        if (s[i] == s[i - 1]) {
+            ans++;
+        }
+    }
+ 
+    cout << ans << endl;
+ 
+    return 0;
+}
